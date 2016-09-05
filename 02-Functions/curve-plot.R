91cfcd8cf45d9d704033ba270586aea8e80ce620
Bozhin Karaivanov <bozhink@gmail.com>
2016-09-05 23:24:09 +0300

    02-Functions is added.

---
+++ 02-Functions/curve-plot.R
@@ -0,0 +1,2 @@
+f<-function(x){3*sin(x/2)+x}
+curve(f,-7,7)
---
+++ 02-Functions/display.R
@@ -0,0 +1,6 @@
+my.display <- function(x) {
+  ##cat("summary of input:", summary(x), sep = "\n")
+  cat("Summary of input: \n") 
+  return(summary(x))
+}
+
